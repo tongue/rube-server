1ad47dc1a4a9fe1fa15fb9ea70b978ae47e627b0
Johan Bergström <johan.bergstrom@isotop.se>
2021-09-16 11:55:45 +0200

    rube server

---
+++ index.js
@@ -0,0 +1,22 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+app.get("/", function (req, res) {
+  io.emit("rube", "Go go go!");
+  res.end();
+});
+
+//Whenever someone connects this gets executed
+io.on("connection", function (socket) {
+  console.log("A user connected");
+
+  //Whenever someone disconnects this piece of code executed
+  socket.on("disconnect", function () {
+    console.log("A user disconnected");
+  });
+});
+
+http.listen(3000, function () {
+  console.log("listening on *:3000");
+});
